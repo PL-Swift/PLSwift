05c2da1cd031091c44961bba7f090897e7481b77
Helge Heß <me@helgehess.eu>
2019-05-12 12:32:35 +0200

    Update Package.swift for Swift 4.2+
    ... and move the files to the new required locations.

--- Package.swift
+++ Package.swift
@@ -1,10 +1,25 @@
+// swift-tools-version:4.2
+//
+//  Package.swift
+//  PL/Swift
+//
+//  Created by Helge Hess on 11.05.18.
+//  Copyright © 2019 ZeeZide. All rights reserved.
+//
 import PackageDescription
 
 let package = Package(
     name: "PLSwift",
 
+    products: [
+      .library(name: "PLSwift", targets: [ "PLSwift" ]),
+    ],
+
     dependencies: [
-      .Package(url: "https://github.com/PL-Swift/CPLSwift.git", 
-               majorVersion: 0, minor: 0)
+      .package(url: "https://github.com/PL-Swift/CPLSwift.git", from: "1.0.2")
+    ],
+    
+    targets: [
+      .target(name: "PLSwift", dependencies: [ "CPLSwift" ])
     ]
 )
